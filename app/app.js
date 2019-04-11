a5886ca744e829b9fdcc28c8ea890c433ad7c7c1
Julia Álvarez <alvarezjulia1@yahoo.es>
2019-04-11 09:51:24 +0200

    Display iteration of an array

--- app/app.js
+++ app/app.js
@@ -6,7 +6,15 @@ app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
     res.render('index', {
-        name: 'Sylvie'
+        name: 'Sylvie',
+        // greeting: '<h1>Hello Sylvie</h1>',
+        bool: Math.round(Math.random())
+    })
+})
+
+app.get('/games', (req, res) => {
+    res.render('games', {
+        gamesList: ['Counter-Strike', 'BF3', 'Sims', 'Street Fighter'].map(el => el.toUpperCase())
     })
 })
 
